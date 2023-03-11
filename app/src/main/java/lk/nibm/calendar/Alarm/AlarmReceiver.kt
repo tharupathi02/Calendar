3a3cad624d5d94bb1812ba6b8441df5a27ed30d1
tharupathi02 <109888408+tharupathi02@users.noreply.github.com>
2023-03-11 15:00:43 +0530

    Added Notification Permission check

--- app/src/main/java/lk/nibm/calendar/Alarm/AlarmReceiver.kt
+++ app/src/main/java/lk/nibm/calendar/Alarm/AlarmReceiver.kt
@@ -1,40 +1,49 @@
 package lk.nibm.calendar.Alarm
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.RingtoneManager
+import android.net.Uri
 import android.os.Build
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.graphics.drawable.toBitmap
 import lk.nibm.calendar.R
-import lk.nibm.calendar.ui.MainActivity
+import lk.nibm.calendar.ui.Dashboard
+
 
 class AlarmReceiver: BroadcastReceiver() {
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onReceive(context: Context?, intent: Intent?) {
         val title = intent?.getStringExtra("title")
         val description = intent?.getStringExtra("description")
         println("AlarmReceiver: $title, $description")
 
-        val i = Intent(context, MainActivity::class.java)
+        val i = Intent(context, Dashboard::class.java)
         intent!!.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         val pendingIntent = PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_IMMUTABLE)
 
         val builder = NotificationCompat.Builder(context!!, "android")
             .setSmallIcon(R.mipmap.ic_launcher)
+            .setLargeIcon(context.resources.getDrawable(R.mipmap.ic_launcher, null).toBitmap())
             .setContentTitle(title)
             .setContentText(description)
+            .setStyle(NotificationCompat.BigTextStyle().bigText(description))
             .setAutoCancel(true)
             .setDefaults(NotificationCompat.DEFAULT_ALL)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setContentIntent(pendingIntent)
+            .setGroup("android")
+            .setGroupSummary(true)
 
         val notificationManager = NotificationManagerCompat.from(context)
 
